a73e8c8fdfb619a6e7bb7f134f38cd26c2dfb1c3
Sunny G <sunny.gonna@gmail.com>
2017-07-11 12:00:52 -0700

    adds style guide colors

--- styles/colors.js
+++ styles/colors.js
@@ -3,17 +3,55 @@
 /* BLACKS AND WHITES */
 export const colorless      = 'rgba(255, 255, 255, 0)';
 export const white          = '#ffffff';
-export const gray__x_light  = '#f5f5f5';
-export const gray__light    = '#e5e5e5';
-export const gray           = '#dcdcdc';
-export const gray__dark     = '#9d9d9d';
-export const gray__x_dark   = '#404041';
 export const black          = '#333333';
 
+export const gray__light    = '#aaaaaa';
+export const gray           = '#969696';
+export const gray__dark     = '#6b6b6b';
+export const gray__x_dark   = '#626366';
+
 /* RAINBOW */
-export const blue           = '#3b96d2';
-export const blue__dark     = '#3280b3';
-export const red            = '#ef5839';
-export const green          = '#68c15f';
+export const purple         = '#834898';
+export const red            = '#e9232e';
+export const red__dark      = '#cf3d27';
+export const orange         = '#ed5941';
+export const yellow         = '#f59831';
+export const green          = '#56b64f';
+export const blue__x_light  = '#e6edf5';
+export const blue__light    = '#4197cf';
+export const blue           = '#3294bc';
+export const blue__dark     = '#2179b4';
+
+/* PRIMARY, SECONDARY AND BACKGROUND */
+export const primary = {
+  text_01       : black,
+  text_02       : gray__dark,
+  nav_01        : blue__dark,
+  nav_02        : red__dark,
+};
+
+export const secondary = {
+  text_01       : gray,
+  text_02       : gray__light,
+  nav_01        : blue__light,
+  nav_02        : orange,
+};
+
+export const background = {
+  ui_01         : white,
+  ui_02         : '#fafafa',
+  ui_03         : '#f5f5f5',
+  ui_04         : blue__x_light,
+};
+
+/* BRAND */
+export const brand = {
+  gray          : gray__x_dark,
+  purple        : purple,
+  red           : red,
+  yellow        : yellow,
+  green         : green,
+  blue          : blue,
+}
 
 /* eslint-enable no-multi-spaces */
