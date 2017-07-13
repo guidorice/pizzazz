86f0bd60e4089b4f679672a1e7d2485d7bc73e01
Sunny G <sunny.gonna@gmail.com>
2017-07-13 13:52:21 -0700

    add spacing

--- styles/colors.js
+++ styles/colors.js
@@ -1,29 +1,29 @@
 /* eslint-disable no-multi-spaces */
 
 /* BLACKS AND WHITES */
-export const colorless      = 'rgba(255, 255, 255, 0)';
-export const white          = '#ffffff';
-export const black          = '#333333';
+export const colorless        = 'rgba(255, 255, 255, 0)';
+export const white            = '#ffffff';
+export const black            = '#333333';
 
-export const gray__xx_light = '#fafafa';
-export const gray__x_light  = '#f5f5f5';
-export const gray__light    = '#f0f0f0';
-export const gray           = '#aaaaaa';
-export const gray__dark     = '#838383';
-export const gray__x_dark   = '#6b6b6b';
-export const gray__xx_dark  = '#626366';
+export const gray__xx_light   = '#fafafa';
+export const gray__x_light    = '#f5f5f5';
+export const gray__light      = '#f0f0f0';
+export const gray             = '#aaaaaa';
+export const gray__dark       = '#838383';
+export const gray__x_dark     = '#6b6b6b';
+export const gray__xx_dark    = '#626366';
 
 /* RAINBOW */
-export const purple         = '#834898';
-export const red            = '#e9232e';
-export const red__dark      = '#c93823';
-export const orange         = '#ed5941';
-export const yellow         = '#f59831';
-export const green          = '#56b64f';
-export const blue__x_light  = '#e6edf5';
-export const blue__light    = '#4197cf';
-export const blue           = '#3294bc';
-export const blue__dark     = '#197b20';
+export const purple           = '#834898';
+export const red              = '#e9232e';
+export const red__dark        = '#c93823';
+export const orange           = '#ed5941';
+export const yellow           = '#f59831';
+export const green            = '#56b64f';
+export const blue__x_light    = '#e6edf5';
+export const blue__light      = '#4197cf';
+export const blue             = '#3294bc';
+export const blue__dark       = '#197b20';
 
 /* PRIMARY, SECONDARY, BACKGROUND, BOUNDARY and BRAND */
 export const primary = {
