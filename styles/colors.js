fce78b2a1e1dd37d9543eaa1e130e1504426f5cd
Sunny G <sunny.gonna@gmail.com>
2017-07-13 12:28:31 -0700

    udpate style guide colors

--- styles/colors.js
+++ styles/colors.js
@@ -7,53 +7,56 @@ export const black          = '#333333';
 
 export const gray__xx_light = '#fafafa';
 export const gray__x_light  = '#f5f5f5';
-export const gray__light    = '#aaaaaa';
-export const gray           = '#969696';
-export const gray__dark     = '#6b6b6b';
-export const gray__x_dark   = '#626366';
+export const gray__light    = '#f0f0f0';
+export const gray           = '#aaaaaa';
+export const gray__dark     = '#838383';
+export const gray__x_dark   = '#6b6b6b';
+export const gray__xx_dark  = '#626366';
 
 /* RAINBOW */
 export const purple         = '#834898';
 export const red            = '#e9232e';
-export const red__dark      = '#cf3d27';
+export const red__dark      = '#c93823';
 export const orange         = '#ed5941';
 export const yellow         = '#f59831';
 export const green          = '#56b64f';
 export const blue__x_light  = '#e6edf5';
 export const blue__light    = '#4197cf';
 export const blue           = '#3294bc';
-export const blue__dark     = '#2179b4';
+export const blue__dark     = '#197b20';
 
-/* PRIMARY, SECONDARY AND BACKGROUND */
+/* PRIMARY, SECONDARY, BACKGROUND, BOUNDARY and BRAND */
 export const primary = {
-  text_01       : black,
-  text_02       : gray__dark,
-  nav_01        : blue__dark,
-  nav_02        : red__dark,
+  dark        : black,
+  blue        : blue__dark,
+  red         : red__dark,
 };
 
 export const secondary = {
-  text_01       : gray,
-  text_02       : gray__light,
-  nav_01        : blue__light,
-  nav_02        : orange,
+  dark        : gray__x_dark,
+  blue        : blue__light,
+  red         : orange,
 };
 
 export const background = {
-  ui_01         : white,
-  ui_02         : gray__xx_light,
-  ui_03         : gray__x_light,
-  ui_04         : blue__x_light,
+  ui_01       : white,
+  ui_02       : gray__xx_light,
+  ui_03       : gray__x_light,
+  ui_04       : gray__light,
 };
 
-/* BRAND */
+export const boundary = {
+  outline     : gray,
+  underline   : gray__dark,
+};
+
 export const brand = {
-  gray          : gray__x_dark,
-  purple        : purple,
-  red           : red,
-  yellow        : yellow,
-  green         : green,
-  blue          : blue,
+  gray        : gray__xx_dark,
+  purple      : purple,
+  red         : red,
+  yellow      : yellow,
+  green       : green,
+  blue        : blue,
 }
 
 /* eslint-enable no-multi-spaces */
