e764f98311b4d5297297d524e65b1daaadecfa2d
Sunny G <sunny.gonna@gmail.com>
2017-07-20 10:22:00 -0700

    adds shadows

---
+++ styles/shadows.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+
+/*
+  map controls
+  thumbnails
+ */
+export const small = css`
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 40);
+`;
+
+/*
+  dropdown menus
+  panels
+ */
+export const medium = css`
+  box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 30);
+`;
+
+/*
+  modals
+  dialogs
+ */
+export const large = css`
+  box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 20);
+`;
+
+/* eslint-enable */
