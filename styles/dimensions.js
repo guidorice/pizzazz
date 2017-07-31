412f3c3f9af25f31380b2596e8967b9c55183af8
Clark Zinzow <clarkzinzow@gmail.com>
2017-07-31 16:10:01 -0600

    Added shadows, fixed dimensions spacing.

--- styles/dimensions.js
+++ styles/dimensions.js
@@ -3,7 +3,7 @@
 /*
   minimum margin between most UI elements
  */
-export const base     = '0.75rem';
+export const base    = '0.75rem';
 
 /*
   height of x-small buttons and form elements
@@ -11,22 +11,22 @@ export const base     = '0.75rem';
   line height at root font size
   icon canvas
  */
-export const x_small  = '1.5rem';
+export const x_small = '1.5rem';
 
 /*
   height of small buttons, form elements, menu items and table rows
  */
-export const small    = '1.875rem';
+export const small   = '1.875rem';
 
 /*
   height of medium buttons, form elements, menu items, and table rows
   paragraph height at root font size
  */
-export const medium   = '2.25rem';
+export const medium  = '2.25rem';
 
 /*
   height of large buttons
  */
-export const large    = '3rem';
+export const large   = '3rem';
 
 /* eslint-enable */
