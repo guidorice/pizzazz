96172865b44f13feccf769bd3df359a9aaac629f
Alex G Rice <alex@descarteslabs.com>
2018-03-09 10:12:55 -0700

    tsc config, build script.

--- styles/index.js
+++ styles/index.js
@@ -1,4 +1,20 @@
-// entry point for library bundle
+// index: entry point for library bundle
+
+// TODO: implement library bundle with tsc + rollup
+
+import * as colors from './colors';
+import * as dimensions from './dimensions';
+import * as fonts from './fonts';
+import * as mixins from './mixins'
+import * as shadows from './shadows';
+
+export {
+    colors,
+    dimensions,
+    fonts,
+    mixins,
+    shadows,
+};
 
 
 
